2fec486344b94e7ebfdde55dccd2fa3fd6af00ad
Kennedy Martin <kennedyjo21@gmail.com>
2019-10-04 15:27:37 -0700

    add initial js front end logic

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business (or back-end) logic:
+
+
+
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function(){
+  $("form#number").submit(function() {
+    event.preventDefault();
+  });
+});

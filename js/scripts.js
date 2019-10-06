04097b457a8c317c08960e352b8b82dc0ff54d32
Kennedy Martin <kennedyjo21@gmail.com>
2019-10-05 19:23:25 -0700

    add initial branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
-Business (or back-end) logic:
-var result = Array.apply(null, {length: N}).map(Number.call, Number)
+// Business (or back-end) logic:
+var result = Array.apply(null, {length: N}).map(Number.call, Number) 
+var string = result.toString() 
+  if str.includes("1") {
+  result.push("Beep!");
+} else if str.includes("2") {
+  result.push ("Boop");
+} else str.includes("3") {
+  result.push ("I'm sorry, Dave. I'm afraid I can't do that.")
+}
     console.log(result);
 
 
-Everything below this line is user interface (or front-end) logic:
+// Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function(){
   $("form#form1").submit(function() {
@@ -13,9 +21,9 @@ $(document).ready(function(){
     // var result = Array.apply(null, {length: N}).map(Number.call, Number)
     // console.log(result);
     
-    if result ()
+    // if result ()
 
-    var string = result.toString();
-    console.log(string);
+    // var string = result.toString();
+    
   });
 });

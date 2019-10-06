90f3df4c615446e845b28f72e5794e288cb69f2d
Kennedy Martin <kennedyjo21@gmail.com>
2019-10-05 19:07:16 -0700

    divide scripts into backend / frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+Business (or back-end) logic:
+var result = Array.apply(null, {length: N}).map(Number.call, Number)
+    console.log(result);
+
+
+Everything below this line is user interface (or front-end) logic:
+
 $(document).ready(function(){
   $("form#form1").submit(function() {
     event.preventDefault();
     var N = parseInt($("#input").val());
-    var result = Array.apply(null, {length: N}).map(Number.call, Number)
-    console.log(result);
+   
+    // var result = Array.apply(null, {length: N}).map(Number.call, Number)
+    // console.log(result);
+    
+    if result ()
+
     var string = result.toString();
     console.log(string);
   });
